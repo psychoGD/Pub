763ffd7d8943061d7fbbe4e01b931e91777ddf1d
abba_ye38 <abba_ye38@STHQ0129-20>
2022-09-17 17:50:57 +0400

    Add project files.

---
+++ WpfApp1/ViewModel/AppViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModel
+{
+    public class AppViewModel
+    {
+        public ObservableCollection<Beer> Beers { get; set; }
+
+        public Beer Beer { get; set; }
+
+    }
+}
